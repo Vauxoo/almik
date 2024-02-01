ebbeadfbf63113d1254c9aa6813e0af49b33bb5f
almiksh <72946052+almiksh@users.noreply.github.com>
2024-02-01 12:27:05 -0600

    almik-dev first commit

---
+++ almik-dev/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Almik Dev",
+
+    'summary': """
+        Almik Dev """,
+
+    'description': """
+        Almik Dev 
+    """,
+
+    'author': "Lytics",
+
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [
+        'views/account_bank_statement.xml',
+    ],
+}
